87d07042c7d9fdcd777f7150473a446be60e900c
Navy <navych@126.com>
2024-06-22 19:39:10 +0800

    change project structure

--- methytools/methytools.py
+++ methytools/methytools.py
@@ -1,3 +1,4 @@
+import argparse
 import pysam
 import gzip
 from importlib import resources
@@ -5,6 +6,7 @@ import pandas as pd
 import numpy as np
 import time
 from pathlib import Path
+from .utils import is_gz_file
 
 
 def load_center(element='tss'):
@@ -123,5 +125,42 @@ def analysis_genomic_element_from_gz(ratio_txt, element, out_matrix, up=50, down
         fo.write('\t'.join([str(round(x, 4)) for x in finals]) + '\n')
 
 
+def main():
+    parser = argparse.ArgumentParser(description='Tools for processing methylation data')
+    subparsers = parser.add_subparsers(dest='sub', required=True, title='command', description='The available commands',
+                                       help='select a sub command to use')
+    # =====================================================================
+    parser_signal = subparsers.add_parser('signal',
+                                          help='Extract the signal value of a region in the genome')
+    parser_signal.add_argument('-i', '--input', required=True, help='The input bed ratio file with header')
+    parser_signal.add_argument('-e', '--element', choices=['tss', 'cgi'], default='tss', help='tss or cgi')
+    parser_signal.add_argument('-u', '--up', default=10000, type=int, help='tss upstream')
+    parser_signal.add_argument('-d', '--down', default=10000, type=int, help='tss downstream')
+    parser_signal.add_argument('-o', '--out', help='The output signal file')
+    parser_signal.add_argument('-v', '--version', action='version', version='0.1')
+    # =====================================================================
+    parser_region_methy = subparsers.add_parser('region-methy',
+                                                help='Extract the signal value of a region in the genome')
+    parser_region_methy.add_argument('-i', '--input', required=True, help='The input bed ratio file with header')
+    parser_region_methy.add_argument('-e', '--element', choices=['tss', 'cgi'], default='tss', help='tss or cgi')
+    parser_region_methy.add_argument('-u', '--up', default=-50, type=int,
+                                     help='the left bounder to the center, center is 0,left is negative, right is positive')
+    parser_region_methy.add_argument('-d', '--down', default=50, type=int,
+                                     help='the right bounder to the center, center is 0,left is negative, right is positive')
+    parser_region_methy.add_argument('-o', '--out', help='The output signal file')
+    parser_region_methy.add_argument('-v', '--version', action='version', version='0.1')
+
+    args = parser.parse_args()
+    if args.sub == 'signal':
+        analysis_genomic_element(args.input, args.element, args.out, args.up, args.down)
+    elif args.sub == 'region-methy':
+        if is_gz_file(args.input):
+            analysis_genomic_element_from_gz(args.input, args.element, args.out, args.up, args.down)
+        else:
+            raise Exception("You should input a bgziped and indexed bed file")
+    else:
+        parser.print_help()
+
+
 if __name__ == '__main__':
-    pass
+    main()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="methytools",
     packages=find_packages(),
-    version="0.0.4",
+    version="0.0.5",
     author="Department of research and development, Zhejiang Gaomei Genomics",
     author_email="it@gomicsgene.com",
     description="methytools is a BS-seq analysis tool suite",
@@ -21,7 +21,7 @@ setup(
     install_requires=['pandas', 'pysam'],
     entry_points={
         'console_scripts': [
-            'methytools=methytools:main',
+            'methytools=methytools.methytools:main',
             'mcomppost=methytools.mcomppost:main',
         ],
     },
